5ec254c4d2731eacb3d1e427b5a4cea5c2cff2f9
ericpeweh <ericpeweh@gmail.com>
2021-11-01 14:31:01 +0700

    Fix unsplash duplicate images and limit
    Modify GetImages controller and logic, more modification at frontend query & actions

--- controllers/api.js
+++ controllers/api.js
@@ -28,15 +28,28 @@ const UNSPLASH4 = axios.create({
 
 // GET /images
 module.exports.getImages = async (req, res) => {
-	const { query } = req.query;
+	const { query, current } = req.query;
+	let totalImages = 0;
 	let limitError = false;
+	let hasMore = true;
 	let result = [];
 
-	const URL = query ? `/photos/random?count=30&query=${query}` : "/photos/random?count=30";
+	if (Number(current) >= 150) {
+		res.status(201).json({ images: [], hasMore: false });
+	}
+
+	const page = Math.ceil(Number(current) / 30) + 1;
+
+	const URL = query
+		? `/search/photos?query=${query}&page=${page}&per_page=30`
+		: `/photos/random?count=30`;
 
 	// 1st TRY
 	try {
 		const res = await UNSPLASH1.get(URL);
+
+		totalImages = res.data.total;
+
 		result = res.data;
 	} catch (err) {
 		if (err.response.data === "Rate Limit Exceeded") {
@@ -50,6 +63,9 @@ module.exports.getImages = async (req, res) => {
 		// 2nd TRY
 		try {
 			const res = await UNSPLASH2.get(URL);
+
+			totalImages = res.data.total;
+
 			result = res.data;
 			limitError = false;
 		} catch (err) {
@@ -66,6 +82,9 @@ module.exports.getImages = async (req, res) => {
 		try {
 			const res = await UNSPLASH3.get(URL);
 			result = res.data;
+
+			totalImages = res.data.total;
+
 			limitError = false;
 		} catch (err) {
 			if (err.response.data === "Rate Limit Exceeded") {
@@ -80,6 +99,9 @@ module.exports.getImages = async (req, res) => {
 		// 4th TRY
 		try {
 			const res = await UNSPLASH4.get(URL);
+
+			totalImages = res.data.total;
+
 			result = res.data;
 			limitError = false;
 		} catch (err) {
@@ -91,19 +113,32 @@ module.exports.getImages = async (req, res) => {
 		}
 	}
 
+	if (totalImages === Number(current)) hasMore = false;
+	if (totalImages < 30) hasMore = false;
+
 	if (limitError) return res.status(400).json({ message: "Image limit exceeded" });
 
 	if (!limitError) {
-		const structuredImages = result.map(image => ({
-			_id: image.id,
-			thumb: image.urls.thumb,
-			regular: image.urls.regular,
-			userLink: image.user.links.html,
-			username: image.user.name,
-			width: image.width,
-			height: image.height
-		}));
+		const structuredImages = query
+			? result.results.map(image => ({
+					_id: image.id,
+					thumb: image.urls.thumb,
+					regular: image.urls.regular,
+					userLink: image.user.links.html,
+					username: image.user.name,
+					width: image.width,
+					height: image.height
+			  }))
+			: result.map(image => ({
+					_id: image.id,
+					thumb: image.urls.thumb,
+					regular: image.urls.regular,
+					userLink: image.user.links.html,
+					username: image.user.name,
+					width: image.width,
+					height: image.height
+			  }));
 
-		res.status(201).json({ images: structuredImages });
+		res.status(201).json({ images: structuredImages, hasMore });
 	}
 };
